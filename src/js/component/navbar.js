578b0a97455b47572eca689f0b2582e3317bd42e
NeliCarrillo <ncx2005@gmail.com>
2024-06-21 21:14:36 -0400

    aja se prendio el navbar papa

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import icono from "../../img/logo.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/login">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-D59E3B mb-3">
+      <Link to="/">
+	  	<img src={icono} alt="Icono" className="navbar-brand mb-0 h1" />
+      </Link>
+      <div className="ml-auto">
+        <ul className="navbar-nav d-flex flex-row ">
+          <li className="nav-item ">
+            <Link to="/menu" className="flex-fill btn btn-outline-dark btn-square">MENÚ</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/nosotros" className="flex-fill btn btn-outline-dark btn-square">NOSOTROS</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/crear-cuenta" className="flex-fill btn  btn-outline-dark btn-square">CREAR CUENTA</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="flex-fill btn btn-outline-dark btn-square">INICIAR SESIÓN</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,7 +3,9 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
+	
 	<div className="text-center mt-5">
+		
 		<h1>Hello Rigo!</h1>
 		<p>
 			<img src={rigoImage} />
