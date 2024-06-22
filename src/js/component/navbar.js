2ff1e755eb1ffbaf3489e1291becee5a9c5b9a65
NeliCarrillo <ncx2005@gmail.com>
2024-06-21 22:24:09 -0400

    avanzando ando

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,23 +6,13 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-D59E3B mb-3">
       <Link to="/">
-	  	<img src={icono} alt="Icono" className="navbar-brand mb-0 h1" />
+        <img src={icono} alt="Icono" className="navbar-brand mb-0 h1" />
       </Link>
-      <div className="ml-auto">
-        <ul className="navbar-nav d-flex flex-row ">
-          <li className="nav-item ">
-            <Link to="/menu" className="flex-fill btn btn-outline-dark btn-square">MENÚ</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/nosotros" className="flex-fill btn btn-outline-dark btn-square">NOSOTROS</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/crear-cuenta" className="flex-fill btn  btn-outline-dark btn-square">CREAR CUENTA</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/login" className="flex-fill btn btn-outline-dark btn-square">INICIAR SESIÓN</Link>
-          </li>
-        </ul>
+      <div className="ml-auto d-flex align-items-stretch">
+        <Link to="/menu" className="btn btn-outline-dark btn-square ">MENÚ</Link>
+        <Link to="/nosotros" className="btn btn-outline-dark btn-square ">NOSOTROS</Link>
+        <Link to="/crear-cuenta" className="btn btn-outline-dark btn-square ">CREAR CUENTA</Link>
+        <Link to="/login" className="btn btn-outline-dark btn-square ">INICIAR SESIÓN</Link>
       </div>
     </nav>
   );
