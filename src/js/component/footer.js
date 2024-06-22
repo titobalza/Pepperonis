630738eaff1a45e4b752625b34974de235c5b38a
LuigiPerezP <perez.luis@correo.unimet.edu.ve>
2024-06-22 17:56:48 -0400

    inicio del footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer mt-auto py-3 text-center" style={{ backgroundColor: "#D22B1C", borderTopLeftRadius: "50%", borderTopRightRadius: "50%", backgroundSize: "100% 100%", backgroundImage: "radial-gradient(circle at top right, #D22B1C 0%, transparent 100%)" }}>
+	  <p>
+		Made with <i className="fa fa-heart text-danger" /> by{" "}
+		<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+	  </p>
 	</footer>
-);
+  );
