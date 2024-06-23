badea48247d2bd5439be50bee180754605e5eb6c
LuigiPerezP <perez.luis@correo.unimet.edu.ve>
2024-06-23 16:34:28 -0400

    acceso rapido

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -13,7 +13,14 @@ export const Footer = () => (
 		width: '100%',
 	}} >
 		<div className="footer-content">
-			// Aquí puedes agregar tus elementos
+		<div className="footer-column t-20">
+				<ul>
+					<li><a href="/">Inicio</a></li>
+					<li><a href="/nosotros">Nosotros</a></li>
+					<li><a href="/crear-cuenta">Crear Cuenta</a></li>
+					<li><a href="/iniciar-sesion">Iniciar Sesión</a></li>
+				</ul>
+			</div>
 		</div>
 	</footer>
 );
