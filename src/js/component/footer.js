59a7626effa139219497660bd87676fbc1056d60
joseigfranc <francisco.jose@correo.unimet.edu.ve>
2024-06-23 12:40:51 -0400

    traje el footer de asierep

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center" style={{ backgroundColor: "#D22B1C", borderTopLeftRadius: "50%", borderTopRightRadius: "50%", backgroundSize: "100% 100%", backgroundImage: "radial-gradient(circle at top right, #D22B1C 0%, transparent 100%)" }}>
-	  <p>
-		Made with <i className="fa fa-heart text-danger" /> by{" "}
-		<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-	  </p>
-	</footer>
-  );
+<footer>
+    <div style={{ backgroundColor: "#D22B1C", position: "relative" }}>
+    	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 460" style={{ position: "absolute", top: 0, width: "100%"}}>
+    		<path fill="#D22B1C" fill-opacity="1" d="M0,224L60,213.3C120,203,240,181,360,186.7C480,192,600,224,720,213.3C840,203,960,149,1080,138.7C1200,128,1320,160,1380,176L1440,192L1440,460L0,460Z"></path>
+		</svg>
+	</div>
+</footer>
+);
