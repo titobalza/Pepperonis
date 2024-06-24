cdb507f970f8a6f54185bcb874ed4377f9e2ae2e
NeliCarrillo <ncx2005@gmail.com>
2024-06-24 16:35:48 -0400

    ahora, si esta logged in cambia el navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import icono from "../../img/logo.png";
 import icono2 from "../../img/unimet.png";
+
 export const Navbar = () => {
+  const [token, setToken] = useState(sessionStorage.getItem("token"));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setToken(sessionStorage.getItem("token"));
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <nav className="navbar navbar-light bg-D59E3B ">
       <div className="sidel d-flex g-1">
-		<img src={icono2} alt="Icono" className="navbar-brand mb-0 h1" />
-		<Link to="/">
-			<img src={icono} alt="Icono" className="navbar-brand mb-0 h1" />
-		</Link>
-	  </div>
+        <img src={icono2} alt="Icono" className="navbar-brand mb-0 h1" />
+        <Link to="/">
+          <img src={icono} alt="Icono" className="navbar-brand mb-0 h1" />
+        </Link>
+      </div>
       <div className="ml-auto d-flex align-items-stretch">
         <ul className="navbar-nav flex-row">
           <li className="nav-item d-flex align-items-stretch">
@@ -19,12 +30,25 @@ export const Navbar = () => {
           <li className="nav-item d-flex align-items-stretch">
             <Link to="/nosotros" className="btn btn-outline-dark btn-square d-flex align-items-center justify-content-center" >NOSOTROS</Link>
           </li>
-          <li className="nav-item d-flex align-items-stretch">
-            <Link to="/signup" className="btn btn-outline-dark btn-square d-flex align-items-center justify-content-center" >CREAR<br />CUENTA</Link>
-          </li>
-          <li className="nav-item d-flex align-items-stretch">
-            <Link to="/login" className="btn btn-outline-dark btn-square d-flex align-items-center justify-content-center bg-D59E3B">INICIAR<br />SESIÓN</Link>
-          </li>
+          {token ? (
+            <>
+              <li className="nav-item d-flex align-items-stretch">
+              <Link to="/pedido" className="btn btn-outline-dark btn-square d-flex align-items-center justify-content-center" >MI PEDIDO</Link>
+            </li>
+            <li className="nav-item d-flex align-items-stretch">
+              <Link to="/login" className="btn btn-outline-dark btn-square d-flex align-items-center justify-content-center" >MI PERFIL</Link>
+            </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item d-flex align-items-stretch">
+                <Link to="/signup" className="btn btn-outline-dark btn-square d-flex align-items-center justify-content-center" >CREAR<br />CUENTA</Link>
+              </li>
+              <li className="nav-item d-flex align-items-stretch">
+                <Link to="/login" className="btn btn-outline-dark btn-square d-flex align-items-center justify-content-center bg-D59E3B">INICIAR<br />SESIÓN</Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
